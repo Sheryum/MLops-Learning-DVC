ac98999f7a0da90c22e94a16986d3c32921eeae2
Sheryum <sherosama23@gmail.com>
2025-07-18 17:01:50 +0100

    This is the first commit of the project.first version of sample data generated and DVC initialized.

---
+++ mycode.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import os
+
+data = {'Name': ['Alice', 'Bob', 'Charlie'],
+        'Age': [25, 30, 35],
+        'City': ['New York', 'Los Angeles', 'Chicago']}
+
+df = pd.DataFrame(data)
+
+
+data_dir = 'data'
+# To check if the directory exists and create it if it doesn't
+os.makedirs(data_dir, exist_ok=True)
+
+#Define the file path
+file_path = os.path.join(data_dir, 'sample_data.csv')
+
+# Save the DataFrame to a CSV file
+df.to_csv(file_path, index=False)
+
+print(f"DataFrame saved to {file_path}")
